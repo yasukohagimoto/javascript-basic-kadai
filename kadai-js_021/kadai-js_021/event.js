281664195dd89534024b43f7324be7863e7eda98
yasukohagimoto <156105126+yasukohagimoto@users.noreply.github.com>
2024-01-23 23:02:33 +0900

    Delete kadai-js_021 directory

--- kadai-js_021/kadai-js_021/event.js
+++
@@ -1,9 +0,0 @@
-const clickBtn = document.getElementById('btn');
-
-clickBtn.addEventListener('click', () => {
-    setTimeout(() => {
-        console.log( text.textContent = 'ボタンをクリックしました');
-    },2000);
-}); 
-
-

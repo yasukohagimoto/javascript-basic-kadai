6feef5a055ac48eb53bc8ff8a0a8b78783bf7a0b
yasukohagimoto <156105126+yasukohagimoto@users.noreply.github.com>
2024-01-23 23:03:51 +0900

    kadao-js_021再アップロード

---
+++ kadai-js_021/event.js
@@ -0,0 +1,9 @@
+const clickBtn = document.getElementById('btn');
+
+clickBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        console.log( text.textContent = 'ボタンをクリックしました');
+    },2000);
+}); 
+
+
